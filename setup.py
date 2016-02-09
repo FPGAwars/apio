a4446bae3751df1f2fddf9f1c70a3046e3aa6b63
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-02-09 15:22:43 +0100

    Add cli

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='apio',
+    version='0.0.1',
+    py_modules=['apio'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        apio=apio:cli
+    ''',
+)
