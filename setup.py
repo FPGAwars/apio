023826910122313630a1600f763a61e3f9bc92f8
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-02-25 10:36:56 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='apio',
     author='Jesús Arroyo Torrens',
     email='jesus.arroyo@bq.com',
-    version='0.0.4.1',
+    version='0.0.4.2',
     packages=['apio'],
     package_data={
         'apio': ['packages/*']
@@ -18,7 +18,7 @@ setup(
     entry_points={
         'console_scripts': ['apio=apio:cli']
     },
-    classifiers=['Development Status :: 1 - Planning',
+    classifiers=['Development Status :: 2 - Pre-Alpha',
                  'Environment :: Console',
                  'Intended Audience :: Developers',
                  'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
