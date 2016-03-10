3d0dac933cd0f77459b49ba41fad358a069b6fc1
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-03-10 11:41:25 +0100

    Added install, uninstall group

--- apio/__init__.py
+++ apio/__init__.py
@@ -7,6 +7,7 @@ from .util import get_systype
 from .execute import run_scons
 from .packages.scons import SconsInstaller
 from .packages.icestorm import IcestormInstaller
+from .packages.driver import DriverInstaller
 
 
 @click.group()
@@ -21,18 +22,42 @@ def debug():
     print('Platform: ' + get_systype())
 
 
-@cli.command('install')
+@cli.group()
 def install():
+    """
+    """
+
+
+@install.command('toolchain')
+def install_toolchain():
     SconsInstaller().install()
     IcestormInstaller().install()
 
 
-@cli.command('uninstall')
+@install.command('drivers')
+def install_drivers():
+    DriverInstaller().install()
+
+
+@cli.group()
 def uninstall():
+    """
+    """
+
+
+@uninstall.command('toolchain')
+def uninstall_toolchain():
     key = raw_input('Are you sure? [Y/N]: ')
     if key == 'y' or key == 'Y':
         SconsInstaller().uninstall()
         IcestormInstaller().uninstall()
+
+
+@uninstall.command('drivers')
+def uninstall_drivers():
+    key = raw_input('Are you sure? [Y/N]: ')
+    if key == 'y' or key == 'Y':
+        DriverInstaller().uninstall()
 
 
 @cli.command('clean')
@@ -58,3 +83,7 @@ def time():
 @cli.command('sim')
 def sim():
     run_scons(['sim'])
+
+
+if __name__ == '__main__':
+    cli()
