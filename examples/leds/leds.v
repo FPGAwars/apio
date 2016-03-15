26f796f905937132207d9d1ba36874c5333cdd44
obijuan <juan@iearobotics.com>
2016-03-15 18:50:59 +0100

    leds example: added simulation files

--- examples/leds/leds.v
+++ examples/leds/leds.v
@@ -1,18 +1,18 @@
 //-- Hello world program
 //-- Turn on all the leds
-module leds(output wire D1,
-            output wire D2,
-            output wire D3,
-            output wire D4,
-            output wire D5);
+module leds(output wire LED0,
+            output wire LED1,
+            output wire LED2,
+            output wire LED3,
+            output wire LED4);
 
 //-- icestick Red leds
-assign D1 = 1'b1;
-assign D2 = 1'b1;
-assign D3 = 1'b1;
-assign D4 = 1'b1;
+assign LED0 = 1'b1;
+assign LED1 = 1'b1;
+assign LED2 = 1'b1;
+assign LED3 = 1'b1;
 
 //-- Green led
-assign D5 = 1;
+assign LED4 = 1'b1;
 
 endmodule
