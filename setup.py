a39ea29cf2f4450e75bbe78931693ffd33d9cdea
obijuan <juan@iearobotics.com>
2016-04-18 19:07:27 +0200

    apio install pio-fpga command implemented: give support for fpgas in platformio

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='apio',
-    version='0.1.0.1',
+    version='0.1.0.2',
     author='Jesús Arroyo Torrens',
     author_email='jesus.jkhlg@gmail.com',
     url='https://github.com/FPGAwars/apio',
@@ -13,7 +13,10 @@ setup(
     package_data={
         'apio': ['SConstruct',
                  'packages/*.py',
-                 'packages/*.rules'],
+                 'packages/*.rules',
+                 'packages/platformio/boards/*',
+                 'packages/platformio/builder/scripts/*.py',
+                 'packages/platformio/platforms/*.py'],
         'examples': ['*/*']
     },
     install_requires=[
