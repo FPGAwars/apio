306eaadb802ed82835a14e330147fb239e71bc6b
obijuan <juan@iearobotics.com>
2016-04-24 17:58:06 +0200

    Example: go-board-leds. Hello world example for the nandland go-board

---
+++ examples/go-board-leds/leds.v
@@ -0,0 +1,16 @@
+//------------------------------------------------------------------
+//-- Hello world example for the nandland go-board
+//-- Turn on all the leds
+//------------------------------------------------------------------
+
+module leds(output wire LED1,
+            output wire LED2,
+            output wire LED3,
+            output wire LED4);
+
+assign LED1 = 1'b1;
+assign LED2 = 1'b1;
+assign LED3 = 1'b1;
+assign LED4 = 1'b1;
+
+endmodule
