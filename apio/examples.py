d95fd83c4e4336b38eac05a49de05ef43fc4ae10
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2016-04-26 15:46:49 +0200

    Add colors to examples command

--- apio/examples.py
+++ apio/examples.py
@@ -7,20 +7,15 @@ import shutil
 
 from os.path import join, isdir, isfile, dirname, basename
 
-try:
-    input = raw_input
-except NameError:
-    pass
-
 
 # -- Error messages
 EXAMPLE_NOT_FOUND_MSG = """
-Sorry, this example does not exist
-Use "apio examples -l" for listing all the available examples"""
+Warning: this example does not exist
+Use `apio examples -l` for listing all the available examples"""
 
 EXAMPLE_OF_USE_CAD = """
 Example of use:
-  apio examples -f leds
+   apio examples -f leds
 Copy the leds example files to the current directory"""
 
 EXAMPLE_DIR_FILE = """
@@ -35,7 +30,7 @@ class Examples(object):
 
     def list_examples(self):
         examples = sorted(os.listdir(self.examples_dir))
-        click.echo('')
+        click.secho('')
         for example in examples:
             example_dir = join(self.examples_dir, example)
             info_path = join(example_dir, 'info')
@@ -43,9 +38,12 @@ class Examples(object):
             if isfile(info_path):
                 with open(info_path, 'r') as info_file:
                     info = info_file.read().replace('\n', '')
-            click.echo(' > ' + example + '  ' + info)
-        click.echo(EXAMPLE_DIR_FILE)
-        click.echo(EXAMPLE_OF_USE_CAD)
+            click.secho(' ' + example, fg='blue', bold=True)
+            click.secho('-' * click.get_terminal_size()[0])
+            click.secho(' ' + info)
+            click.secho('')
+        click.secho(EXAMPLE_DIR_FILE, fg='green')
+        click.secho(EXAMPLE_OF_USE_CAD, fg='green')
         return
 
     def copy_example_dir(self, example):
@@ -54,17 +52,19 @@ class Examples(object):
 
         if isdir(local_example_path):
             if isdir(example_path):
-                click.echo('Warning: ' + example + ' directory already exists')
-                key = input('Do you want to replace it? [Y/N]: ')
-                if key == 'y' or key == 'Y':
+                click.secho(
+                    'Warning: ' + example + ' directory already exists',
+                    fg='yellow')
+                if click.confirm('Do you want to replace it?'):
                     shutil.rmtree(example_path)
                     self._copy_dir(example, local_example_path, example_path)
             elif isfile(example_path):
-                click.echo('Warning: ' + example + ' is already a file')
+                click.secho(
+                    'Warning: ' + example + ' is already a file', fg='yellow')
             else:
                 self._copy_dir(example, local_example_path, example_path)
         else:
-            click.echo(EXAMPLE_NOT_FOUND_MSG)
+            click.secho(EXAMPLE_NOT_FOUND_MSG, fg='yellow')
 
     def copy_example_files(self, example):
         example_path = os.getcwd()
@@ -73,27 +73,37 @@ class Examples(object):
         if isdir(local_example_path):
             self._copy_files(example, local_example_path, example_path)
         else:
-            click.echo(EXAMPLE_NOT_FOUND_MSG)
+            click.secho(EXAMPLE_NOT_FOUND_MSG, fg='yellow')
 
     def _copy_files(self, example, src_path, dest_path):
-        click.echo(' Copying ' + example + ' example')
+        click.secho('Copying ' + example + ' example files ...')
         example_files = glob.glob(join(src_path, '*'))
         for f in example_files:
             filename = basename(f)
             if filename != 'info':
                 if isfile(join(dest_path, filename)):
-                    click.echo('Warning: ' + filename + ' file already exists')
-                    key = input('Do you want to replace it? [Y/N]: ')
-                    if key == 'y' or key == 'Y':
+                    click.secho(
+                        'Warning: ' + filename + ' file already exists',
+                        fg='yellow')
+                    if click.confirm('Do you want to replace it?'):
                         shutil.copy(f, dest_path)
                 elif isdir(join(dest_path, filename)):
-                    click.echo('Warning: ' + filename + ' is already a directory')
+                    click.secho(
+                        'Warning: ' + filename + ' is already a directory',
+                        fg='yellow')
+                    return
                 else:
                     shutil.copy(f, dest_path)
+        click.secho(
+            'Example files \'' + example + '\' have been successfully created!',
+            fg='green')
 
     def _copy_dir(self, example, src_path, dest_path):
-        click.echo(' Creating ' + example + ' directory')
+        click.secho('Creating ' + example + ' directory ...')
         shutil.copytree(src_path, dest_path)
+        click.secho(
+            'Example \'' + example + '\' has been successfully created!',
+            fg='green')
 
     def examples_of_use_cad(self):
         return EXAMPLE_OF_USE_CAD
