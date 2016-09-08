80ed28674e7c9d9333d4114c839265d65fee74f3
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2016-09-08 13:26:03 +0200

    Fix SCons bug

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='apio',
-    version='0.1.6',
+    version='0.1.6.1',
     description='Experimental micro-ecosystem for open FPGAs',
     author='Jesús Arroyo Torrens',
     author_email='jesus.jkhlg@gmail.com',
