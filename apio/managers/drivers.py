5b80f6acb0153eabc97903ec4b6583a418733078
JESUS ARROYO TORRENS <jesus.arroyo@bq-ms.local>
2016-11-02 17:14:25 +0100

    Add Windows drivers configuration

--- apio/managers/drivers.py
+++ apio/managers/drivers.py
@@ -4,6 +4,7 @@
 # -- Author Jesús Arroyo
 # -- Licence GPLv2
 
+import os
 import click
 import subprocess
 
@@ -12,6 +13,30 @@ from os.path import join, dirname, isfile, isdir
 from apio import util
 
 platform = util.get_systype()
+
+FTDI_INSTALL_DRIVER_INSTRUCTIONS = """
+   FTDI driver installation:
+   Usage instructions
+
+      1. Connect the FTDI FPGA board
+      2. Select (Interface 0)
+      3. Replace driver by "libusbK"
+      4. Reconnect the board
+      5. Check `apio system --lsftdi`
+
+   NOTE: Windows 10 + USB 3.0 also requires:
+
+      Select (Interface 1) and replace driver by "libusb-win32"
+"""
+
+FTDI_UNINSTALL_DRIVER_INSTRUCTIONS = """
+   FTDI driver uninstallation:
+   Usage instructions
+
+      1. Find the FPGA USB Devices
+      2. Right click
+      3. Select "Uninstall"
+      4. Accept the dialog"""
 
 
 class Drivers(object):  # pragma: no cover
@@ -22,19 +47,19 @@ class Drivers(object):  # pragma: no cover
 
     def enable(self):
         if 'linux' in platform:
-            self._enable_linux()
+            return self._enable_linux()
         elif 'darwin' in platform:
-            self._enable_darwin()
+            return self._enable_darwin()
         elif 'windows' in platform:
-            self._enable_windows()
+            return self._enable_windows()
 
     def disable(self):
         if 'linux' in platform:
-            self._disable_linux()
+            return self._disable_linux()
         elif 'darwin' in platform:
-            self._disable_darwin()
+            return self._disable_darwin()
         elif 'windows' in platform:
-            self._disable_windows()
+            return self._disable_windows()
 
     def _enable_linux(self):
         click.secho('Configure FTDI drivers for FPGA')
@@ -73,35 +98,41 @@ class Drivers(object):  # pragma: no cover
         click.secho('FPGA drivers disabled', fg='green')
 
     def _enable_windows(self):
-        click.secho('Launch FTDI driver configuration tool')
-
-        result = self._run('zadig.exe')
-
-        if isinstance(result, int):
-            return result
-
-        if result:
-            return result['returncode']
-
-    def _disable_windows(self):
-        pass
-
-    def _run(self, command):
-        result = {}
         drivers_base_dir = util.get_package_dir('tools-drivers')
         drivers_bin_dir = join(drivers_base_dir, 'bin')
+        drivers_share_dir = join(drivers_base_dir, 'share')
+        zadig_ini_path = join(drivers_share_dir, 'zadig.ini')
+        zadig_ini = 'zadig.ini'
 
-        if isdir(drivers_bin_dir):
-            result = util.exec_command(
-                join(drivers_bin_dir, command),
-                stdout=util.AsyncPipe(self._on_run_out),
-                stderr=util.AsyncPipe(self._on_run_out)
-                )
-        else:
-            util._check_package('tools-drivers')
-            return 1
+        try:
+            if isdir(drivers_bin_dir):
+                click.secho('Launch drivers configuration tool')
+                click.secho(FTDI_INSTALL_DRIVER_INSTRUCTIONS, fg='yellow')
+                # Copy zadig.ini
+                with open(zadig_ini, 'w') as ini_file:
+                    with open(zadig_ini_path, 'r') as local_ini_file:
+                        ini_file.write(local_ini_file.read())
 
+                result = util.exec_command(join(drivers_bin_dir, 'zadig.exe'))
+                click.secho('FPGA drivers configuration finished', fg='green')
+            else:
+                util._check_package('drivers')
+                result = 1
+        except Exception as e:
+            click.secho('Error: ' + str(e), fg='red')
+            result = 1
+        finally:
+            # Remove zadig.ini
+            if isfile(zadig_ini):
+                os.remove(zadig_ini)
+
+        if not isinstance(result, int):
+            result = result['returncode']
         return result
 
-    def _on_run_out(self, line):
-        click.secho(line)
+    def _disable_windows(self):
+        click.secho('Launch device manager')
+        click.secho(FTDI_UNINSTALL_DRIVER_INSTRUCTIONS, fg='yellow')
+
+        result = util.exec_command('mmc devmgmt.msc')
+        return result['returncode']
