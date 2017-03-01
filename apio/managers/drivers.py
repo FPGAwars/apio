de561e6470ed006db6fb57f5e5e5295443dbe620
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2017-03-01 15:35:41 +0100

    Improve Mac OS drivers management

--- apio/managers/drivers.py
+++ apio/managers/drivers.py
@@ -65,11 +65,11 @@ class Drivers(object):  # pragma: no cover
 
     def pre_upload(self):
         if 'darwin' in platform:
-            return self._pre_upload_darwin()
+            self._pre_upload_darwin()
 
     def post_upload(self):
         if 'darwin' in platform:
-            return self._post_upload_darwin()
+            self._post_upload_darwin()
 
     def _enable_linux(self):
         click.secho('Configure FTDI drivers for FPGA')
@@ -104,16 +104,20 @@ class Drivers(object):  # pragma: no cover
         else:
             click.secho('Enable FTDI drivers for FPGA')
             subprocess.call(['brew', 'update'])
-            subprocess.call(['brew', 'install', 'libftdi'])
-            subprocess.call(['brew', 'link', '--overwrite', 'libftdi'])
-            subprocess.call(['brew', 'install', 'libffi'])
-            subprocess.call(['brew', 'link', '--overwrite', 'libffi'])
+            subprocess.call(['brew', 'install', '--force', 'libftdi'])
+            subprocess.call(['brew', 'unlink', 'libftdi'])
+            subprocess.call(['brew', 'link', '--force', 'libftdi'])
+            subprocess.call(['brew', 'install', '--force', 'libffi'])
+            subprocess.call(['brew', 'unlink', 'libffi'])
+            subprocess.call(['brew', 'link', '--force', 'libffi'])
             self.profile.add_setting('macos_drivers', True)
+            self.profile.save()
             click.secho('FPGA drivers enabled', fg='green')
 
     def _disable_darwin(self):
         click.secho('Disable FTDI drivers\' configuration')
         self.profile.add_setting('macos_drivers', False)
+        self.profile.save()
         click.secho('FPGA drivers disabled', fg='green')
 
     def _pre_upload_darwin(self):
@@ -135,7 +139,7 @@ class Drivers(object):  # pragma: no cover
                 subprocess.call(['sudo', 'kextload', '-b', self.driverC])
 
     def _check_driver_darwin(self, driver):
-        return subprocess.call(['sudo', 'kextstat', '|', 'grep', '-c', driver])
+        return driver in subprocess.check_output(['kextstat'])
 
     def _enable_windows(self):
         drivers_base_dir = util.get_package_dir('tools-drivers')
