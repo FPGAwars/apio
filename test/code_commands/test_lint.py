a85252825e6a282479f4bc1468a4cbf3d434d839
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2018-04-08 17:32:48 +0200

    Add test for lint command

---
+++ test/code_commands/test_lint.py
@@ -0,0 +1,10 @@
+from apio.commands.lint import cli as cmd_lint
+
+
+def test_lint(clirunner, configenv):
+    with clirunner.isolated_filesystem():
+        configenv()
+        result = clirunner.invoke(cmd_lint)
+        assert result.exit_code != 0
+        if result.exit_code == 1:
+            assert 'apio install verilator' in result.output
