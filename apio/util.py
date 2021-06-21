c1c3ad5600580586f366bb287775a0ddfc33e7b2
Obijuan <obijuan.cube@gmail.com>
2021-06-21 17:59:28 +0200

    More pylint messages fixed

--- apio/util.py
+++ apio/util.py
@@ -1,3 +1,4 @@
+"""DOC: TODO"""
 # -*- coding: utf-8 -*-
 # -- This file is part of the Apio project
 # -- (C) 2016-2018 FPGAwars
@@ -8,24 +9,28 @@
 # ---- (C) 2014-2016 Ivan Kravets <me@ikravets.com>
 # ---- Licence Apache v2
 
+import sys
 import os
 import re
-import sys
-import jwt
 import json
-import click
 import locale
 import platform
 import subprocess
-import semantic_version
 from threading import Thread
 from os.path import expanduser, isdir, isfile, join, dirname, exists, normpath
 
-from apio import LOAD_CONFIG_DATA
-
+import jwt
+import click
+import semantic_version
+from serial.tools.list_ports import comports
 import requests
 
+from apio import LOAD_CONFIG_DATA
+
+
+
 requests.packages.urllib3.disable_warnings()
+
 
 # Python3 compat
 if sys.version_info > (3, 0):
@@ -33,17 +38,20 @@ if sys.version_info > (3, 0):
 
 
 class ApioException(Exception):
+    """DOC: TODO"""
 
     MESSAGE = None
 
     def __str__(self):  # pragma: no cover
         if self.MESSAGE:
             return self.MESSAGE.format(*self.args)
-        else:
-            return Exception.__str__(self)
+
+        return Exception.__str__(self)
 
 
 class AsyncPipe(Thread):  # pragma: no cover
+    """DOC: TODO"""
+
     def __init__(self, outcallback=None):
         Thread.__init__(self)
         self.outcallback = outcallback
@@ -55,12 +63,18 @@ class AsyncPipe(Thread):  # pragma: no cover
         self.start()
 
     def get_buffer(self):
+        """DOC: TODO"""
+
         return self._buffer
 
     def fileno(self):
+        """DOC: TODO"""
+
         return self._fd_write
 
     def run(self):
+        """DOC: TODO"""
+
         for line in iter(self._pipe_reader.readline, ""):
             line = line.strip()
             self._buffer.append(line)
@@ -69,11 +83,15 @@ class AsyncPipe(Thread):  # pragma: no cover
         self._pipe_reader.close()
 
     def close(self):
+        """DOC: TODO"""
+
         os.close(self._fd_write)
         self.join()
 
 
 def get_systype():
+    """DOC: TODO"""
+
     type_ = platform.system().lower()
     arch = platform.machine().lower()
     if type_ == "windows":
@@ -92,23 +110,26 @@ except Exception:
     UTF = False
 
 
-def unicoder(p):
+def unicoder(string):
     """Make sure a Unicode string is returned"""
-    if isinstance(p, unicode):
-        return p
-    if isinstance(p, str):
-        return decoder(p)
-    else:
-        return unicode(decoder(p))
+    if isinstance(string, unicode):
+        return string
+
+    if isinstance(string, str):
+        return decoder(string)
+
+    return unicode(decoder(string))
 
 
-def decoder(p):
+def decoder(string):
+    """DOC: TODO"""
+
     if UTF:
         try:
-            return p.decode("utf-8")
+            return string.decode("utf-8")
         except Exception:
-            return p.decode(codepage)
-    return p.decode(codepage)
+            return string.decode(codepage)
+    return string.decode(codepage)
 
 
 def safe_join(*paths):
@@ -123,14 +144,14 @@ def safe_join(*paths):
 
 
 def _get_config_data():
-    config_data = None
+    _config_data = None
     if LOAD_CONFIG_DATA:
         filepath = safe_join(os.sep, "etc", "apio.json")
         if isfile(filepath):
-            with open(filepath, "r") as f:
+            with open(filepath, "r") as file:
                 # Load the JSON file
-                config_data = json.loads(f.read())
-    return config_data
+                _config_data = json.loads(file.read())
+    return _config_data
 
 
 config_data = _get_config_data()
@@ -146,6 +167,8 @@ def _get_projconf_option_dir(name, default=None):
 
 
 def get_home_dir():
+    """DOC: TODO"""
+
     home_dir = _get_projconf_option_dir("home_dir", "~/.apio")
     home_dir = re.sub(r"\~", expanduser("~").replace("\\", "/"), home_dir)
 
@@ -155,7 +178,7 @@ def get_home_dir():
         path = _create_path(paths)
         if not path:
             click.secho("Error: no usable home directory " + path, fg="red")
-            exit(1)
+            sys.exit(1)
     return path
 
 
@@ -167,10 +190,9 @@ def _check_writable(paths):
                 # Path is writable
                 ret = path
                 break
-            else:
-                click.secho(
-                    "Warning: can't write in path " + path, fg="yellow"
-                )
+            click.secho(
+              "Warning: can't write in path " + path, fg="yellow"
+            )
     return ret
 
 
@@ -189,6 +211,8 @@ def _create_path(paths):
 
 
 def get_package_dir(pkg_name):
+    """DOC: TODO"""
+
     home_dir = _get_projconf_option_dir("pkg_dir", "")
     if not home_dir:
         home_dir = _get_projconf_option_dir("home_dir", "~/.apio")
@@ -204,15 +228,21 @@ def get_package_dir(pkg_name):
 
 
 def get_project_dir():
+    """DOC: TODO"""
+
     return os.getcwd()
 
 
 def call(cmd):
+    """DOC: TODO"""
+
     setup_environment()
     return subprocess.call(cmd, shell=True)
 
 
 def setup_environment():
+    """DOC: TODO"""
+
     base_dir = {
         "scons": get_package_dir("tool-scons"),
         "system": get_package_dir("tools-system"),
@@ -280,6 +310,7 @@ def setup_environment():
 
 
 def resolve_packages(packages, installed_packages, spec_packages):
+    """DOC: TODO"""
 
     base_dir = {
         "scons": get_package_dir("tool-scons"),
@@ -364,7 +395,6 @@ def resolve_packages(packages, installed_packages, spec_packages):
             base_dir.get("yosys"), "share", "yosys"
         )
 
-        global scons_command
         scons_command = [
             normpath(sys.executable),
             safe_join(bin_dir["scons"], "scons"),
@@ -374,6 +404,8 @@ def resolve_packages(packages, installed_packages, spec_packages):
 
 
 def check_package(name, version, spec_version, path):
+    """DOC: TODO"""
+
     # Apio package 'gtkwave' only exists for Windows.
     # Linux and MacOS user must install the native GTKWave.
     if name == "gtkwave" and platform.system() != "Windows":
@@ -395,14 +427,15 @@ def check_package(name, version, spec_version, path):
 
 
 def check_package_version(version, spec_version):
-    try:
-        spec = semantic_version.Spec(spec_version)
-        return semantic_version.Version(version) in spec
-    except ValueError:
-        pass
+    """DOC: TODO"""
+
+    spec = semantic_version.Spec(spec_version)
+    return semantic_version.Version(version) in spec
 
 
 def show_package_version_warning(name, version, spec_version):
+    """DOC: TODO"""
+
     message = (
         "Warning: package '{0}' version {1}\n"
         "does not match the semantic version {2}"
@@ -411,11 +444,15 @@ def show_package_version_warning(name, version, spec_version):
 
 
 def show_package_path_error(name):
+    """DOC: TODO"""
+
     message = "Error: package '{}' is not installed".format(name)
     click.secho(message, fg="red")
 
 
 def show_package_install_instructions(name):
+    """DOC: TODO"""
+
     if config_data and _check_apt_get():  # /etc/apio.json file exists
         click.secho(
             "Please run:\n" "   apt-get install apio-{}".format(name),
@@ -441,6 +478,8 @@ def _check_apt_get():
 
 
 def get_package_version(name, profile):
+    """DOC: TODO"""
+
     version = ""
     if name in profile.packages:
         version = profile.packages.get(name).get("version")
@@ -448,6 +487,8 @@ def get_package_version(name, profile):
 
 
 def get_package_spec_version(name, resources):
+    """DOC: TODO"""
+
     spec_version = ""
     if name in resources.distribution.get("packages"):
         spec_version = resources.distribution.get("packages").get(name)
@@ -455,10 +496,14 @@ def get_package_spec_version(name, resources):
 
 
 def change_filemtime(path, time):
+    """DOC: TODO"""
+
     os.utime(path, (time, time))
 
 
 def exec_command(*args, **kwargs):  # pragma: no cover
+    """DOC: TODO"""
+
     result = {"out": None, "err": None, "returncode": None}
 
     default = dict(
@@ -470,19 +515,19 @@ def exec_command(*args, **kwargs):  # pragma: no cover
     kwargs = default
 
     try:
-        p = subprocess.Popen(*args, **kwargs)
-        result["out"], result["err"] = p.communicate()
-        result["returncode"] = p.returncode
+        proc = subprocess.Popen(*args, **kwargs)
+        result["out"], result["err"] = proc.communicate()
+        result["returncode"] = proc.returncode
     except KeyboardInterrupt:
         click.secho("Aborted by user", fg="red")
-        exit(1)
-    except Exception as e:
-        click.secho(str(e), fg="red")
-        exit(1)
+        sys.exit(1)
+    except Exception as exc:
+        click.secho(str(exc), fg="red")
+        sys.exit(1)
     finally:
-        for s in ("stdout", "stderr"):
-            if isinstance(kwargs[s], AsyncPipe):
-                kwargs[s].close()
+        for std in ("stdout", "stderr"):
+            if isinstance(kwargs[std], AsyncPipe):
+                kwargs[std].close()
 
     _parse_result(kwargs, result)
 
@@ -490,24 +535,26 @@ def exec_command(*args, **kwargs):  # pragma: no cover
 
 
 def _parse_result(kwargs, result):
-    for s in ("stdout", "stderr"):
-        if isinstance(kwargs[s], AsyncPipe):
-            result[s[3:]] = "\n".join(kwargs[s].get_buffer())
+    for std in ("stdout", "stderr"):
+        if isinstance(kwargs[std], AsyncPipe):
+            result[std[3:]] = "\n".join(kwargs[std].get_buffer())
 
-    for k, v in result.items():
-        if v and isinstance(v, unicode):
+    for k, value in result.items():
+        if value and isinstance(value, unicode):
             result[k].strip()
 
 
 def get_pypi_latest_version():
-    r = None
+    """DOC: TODO"""
+
+    req = None
     version = None
     try:
-        r = requests.get("https://pypi.python.org/pypi/apio/json")
-        version = r.json().get("info").get("version")
-        r.raise_for_status()
-    except requests.exceptions.ConnectionError as e:
-        error_message = str(e)
+        req = requests.get("https://pypi.python.org/pypi/apio/json")
+        version = req.json().get("info").get("version")
+        req.raise_for_status()
+    except requests.exceptions.ConnectionError as exc:
+        error_message = str(exc)
         if "NewConnectionError" in error_message:
             click.secho(
                 "Error: could not connect to Pypi.\n"
@@ -516,19 +563,23 @@ def get_pypi_latest_version():
             )
         else:
             click.secho(error_message, fg="red")
-    except Exception as e:
-        click.secho("Error: " + str(e), fg="red")
+    except Exception as exc:
+        click.secho("Error: " + str(exc), fg="red")
     finally:
-        if r:
-            r.close()
+        if req:
+            req.close()
     return version
 
 
 def get_folder(folder):
+    """DOC: TODO"""
+
     return safe_join(dirname(__file__), folder)
 
 
 def mkdir(path):
+    """DOC: TODO"""
+
     path = dirname(path)
     if not exists(path):
         try:
@@ -538,6 +589,8 @@ def mkdir(path):
 
 
 def check_dir(_dir):
+    """DOC: TODO"""
+
     if _dir is None:
         _dir = os.getcwd()
 
@@ -546,7 +599,7 @@ def check_dir(_dir):
             "Error: project directory is already a file: {0}".format(_dir),
             fg="red",
         )
-        exit(1)
+        sys.exit(1)
 
     if not exists(_dir):
         try:
@@ -563,9 +616,9 @@ def command(function):
         exit_code = 1
         try:
             exit_code = function(*args, **kwargs)
-        except Exception as e:
-            if str(e):
-                click.secho("Error: " + str(e), fg="red")
+        except Exception as exc:
+            if str(exc):
+                click.secho("Error: " + str(exc), fg="red")
         finally:
             return exit_code
 
@@ -573,11 +626,13 @@ def command(function):
 
 
 def decode(text):
+    """DOC: TODO"""
+
     return jwt.decode(text, "secret", algorithm="HS256")
 
 
 def get_serial_ports():
-    from serial.tools.list_ports import comports
+    """DOC: TODO"""
 
     result = []
 
@@ -593,19 +648,23 @@ def get_serial_ports():
 
 
 def get_tinyprog_meta():
-    command = join(get_bin_dir(), "tinyprog")
-    result = exec_command([command, "--pyserial", "--meta"])
+    """DOC: TODO"""
+
+    _command = join(get_bin_dir(), "tinyprog")
+    result = exec_command([_command, "--pyserial", "--meta"])
     try:
         out = unicoder(result.get("out", ""))
         if out:
             return json.loads(out)
-    except Exception as e:
-        print(e)
+    except Exception as exc:
+        print(exc)
         return []
     return []
 
 
 def get_bin_dir():
+    """DOC: TODO"""
+
     candidate = dirname(sys.modules["__main__"].__file__)
     # Windows + virtualenv = 💩
     # In this case the main file is: venv/Scripts/apio.exe/__main__.py!
@@ -613,9 +672,11 @@ def get_bin_dir():
     # So here we go with the workaround:
     if candidate.endswith(".exe"):
         return dirname(candidate)
-    else:
-        return candidate
+
+    return candidate
 
 
 def get_python_version():
+    """DOC: TODO"""
+
     return "{0}.{1}".format(sys.version_info[0], sys.version_info[1])
