683d7352e128a09ac9ac08d3dbe1f5b01c8cbcab
obijuan <obijuan.cube@gmail.com>
2021-06-26 13:30:53 +0200

    extra packages are now separated. Version bumped to 0.6.7

--- apio/__init__.py
+++ apio/__init__.py
@@ -9,7 +9,7 @@
 # - Information for the Distribution package
 # --------------------------------------------
 
-VERSION = (0, 6, 6)
+VERSION = (0, 6, 7)
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "apio"
