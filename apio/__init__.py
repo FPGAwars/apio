2ab066cd83a5e01d67a021961bef08960a53b4fa
obijuan <obijuan.cube@gmail.com>
2021-06-26 12:53:09 +0200

    Extra packages added. Version bumped to 0.6.5

--- apio/__init__.py
+++ apio/__init__.py
@@ -9,7 +9,7 @@
 # - Information for the Distribution package
 # --------------------------------------------
 
-VERSION = (0, 6, 4)
+VERSION = (0, 6, 5)
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "apio"
