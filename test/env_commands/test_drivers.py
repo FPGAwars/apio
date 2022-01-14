1061c48b7c16aa0b23b4fe4717d9b7f560dbce7e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-14 21:01:41 +0800

    Delete test directory

--- test/env_commands/test_drivers.py
+++
@@ -1,6 +0,0 @@
-from apio.commands.drivers import cli as cmd_drivers
-
-
-def test_drivers(clirunner, validate_cliresult):
-    result = clirunner.invoke(cmd_drivers)
-    validate_cliresult(result)
