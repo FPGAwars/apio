bf4de0fb91830eb50b89e6c8d7b85e9fdca7ac92
Obijuan <obijuan.cube@gmail.com>
2023-10-03 08:42:34 +0200

    Version set to 0.8.4

--- apio/__init__.py
+++ apio/__init__.py
@@ -9,7 +9,7 @@
 # - Information for the Distribution package
 # --------------------------------------------
 
-VERSION = (0, 8, 3)
+VERSION = (0, 8, 4)
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "apio"
