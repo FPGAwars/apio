f60ec5cc58eae2bf884fa75a9789b55bbff87859
Obijuan <obijuan.cube@gmail.com>
2024-02-20 09:01:51 +0100

    apio init: add --top-module parameter

--- apio/commands/init.py
+++ apio/commands/init.py
@@ -10,17 +10,27 @@ import click
 from apio.managers.project import Project
 
 
+# R0913: Too many arguments (6/5)
+# pylint: disable=R0913
 @click.command("init")
 @click.pass_context
-@click.option(
-    "-s", "--scons", is_flag=True, help="Create default SConstruct file."
-)
 @click.option(
     "-b",
     "--board",
     type=str,
     metavar="board",
     help="Create init file with the selected board.",
+)
+@click.option(
+    "-t",
+    "--top-module",
+    type=str,
+    metavar="top_module",
+    default="main",
+    help="Set the top_module in the init file",
+)
+@click.option(
+    "-s", "--scons", is_flag=True, help="Create default SConstruct file."
 )
 @click.option(
     "-p",
@@ -35,12 +45,12 @@ from apio.managers.project import Project
     is_flag=True,
     help="Automatically answer YES to all the questions.",
 )
-def cli(ctx, board, scons, project_dir, sayyes):
+def cli(ctx, board, top_module, scons, project_dir, sayyes):
     """Manage apio projects."""
 
     if scons:
         Project().create_sconstruct(project_dir, "ice40", sayyes)
     elif board:
-        Project().create_ini(board, project_dir, sayyes)
+        Project().create_ini(board, top_module, project_dir, sayyes)
     else:
         click.secho(ctx.get_help())
