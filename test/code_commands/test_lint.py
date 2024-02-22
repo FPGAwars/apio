de9e60e1cb771e457a8ad9e8b90542abb37a34a0
Obijuan <obijuan.cube@gmail.com>
2024-02-22 19:52:37 +0100

    refactor test_lint.py

--- test/code_commands/test_lint.py
+++ test/code_commands/test_lint.py
@@ -1,10 +1,20 @@
+"""
+  Test for the "apio lint" command
+"""
 from apio.commands.lint import cli as cmd_lint
 
 
 def test_lint(clirunner, configenv):
+    """Test: apio lint
+    when no apio.ini file is given
+    No additional parameters are given
+    """
+
     with clirunner.isolated_filesystem():
+
+        # -- Config the environment (conftest.configenv())
         configenv()
-        result = clirunner.invoke(cmd_lint, ['--board', 'icezum'])
+
+        # -- Execute "apio lint"
+        result = clirunner.invoke(cmd_lint, ['--board', 'alhambra-ii'])
         assert result.exit_code != 0
-        if result.exit_code == 1:
-            assert 'apio install verilator' in result.output
