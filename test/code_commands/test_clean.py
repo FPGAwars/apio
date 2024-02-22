b644a310dd71044980db9d4232a7af8481e71e31
Obijuan <obijuan.cube@gmail.com>
2024-02-22 19:42:45 +0100

    refactor test_clean.py

--- test/code_commands/test_clean.py
+++ test/code_commands/test_clean.py
@@ -1,8 +1,54 @@
+"""
+  Test for the "apio clean" command
+"""
+
+# -- apio clean entry point
 from apio.commands.clean import cli as cmd_clean
+
+# -- apio init entry point
+from apio.commands.init import cli as cmd_init
 
 
 def test_clean(clirunner, configenv):
+    """Test: apio clean
+    when no apio.ini file is given
+    No additional parameters are given
+    """
+
     with clirunner.isolated_filesystem():
+
+        # -- Config the environment (conftest.configenv())
         configenv()
+
+        # -- Execute "apio clean"
+        result = clirunner.invoke(cmd_clean)
+
+        # -- It is an error. Exit code should not be 0
+        assert result.exit_code != 0
+        assert "Info: No apio.ini file" in result.output
+        assert "Error: insufficient arguments: missing board" in result.output
+
+        # -- Execute "apio clean --board alhambra-ii"
+        result = clirunner.invoke(cmd_clean, ["--board", "alhambra-ii"])
+        assert result.exit_code != 0
+
+
+def test_clean_init(clirunner, configenv):
+    """Test: apio clean
+    when there is an apio.ini file
+    """
+
+    with clirunner.isolated_filesystem():
+
+        # -- Config the environment (conftest.configenv())
+        configenv()
+
+        # apio init --board icezum
+        result = clirunner.invoke(cmd_init, ["--board", "alhambra-ii"])
+        assert result.exit_code == 0
+        assert "Creating apio.ini file ..." in result.output
+        assert "has been successfully created!" in result.output
+
+        # --- Execute "apio clean"
         result = clirunner.invoke(cmd_clean)
         assert result.exit_code != 0
