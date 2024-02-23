f8e1f64120e5b7ee5b232e5ee965f5f8ce76bc71
Obijuan <obijuan.cube@gmail.com>
2024-02-23 22:05:39 +0100

    refactor test_verify.py

--- test/code_commands/test_verify.py
+++ test/code_commands/test_verify.py
@@ -1,10 +1,25 @@
+"""
+  Test for the "apio verify" command
+"""
+
+# -- apio verify entry point
 from apio.commands.verify import cli as cmd_verify
 
 
 def test_verify(clirunner, configenv):
+    """Test: apio verify
+    when no apio.ini file is given
+    No additional parameters are given
+    """
+
     with clirunner.isolated_filesystem():
+
+        # -- Config the environment (conftest.configenv())
         configenv()
+
+        # -- Execute "apio verify"
         result = clirunner.invoke(cmd_verify, ['--board', 'icezum'])
+
+        # -- Check the result
         assert result.exit_code != 0
-        #if result.exit_code == 1:
-        #    assert 'apio install oss-cad-suite' in result.output
+        assert 'apio install oss-cad-suite' in result.output
