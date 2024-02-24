a02fb5c00a68cf81801da20c61f26853b6308c5c
Obijuan <obijuan.cube@gmail.com>
2024-02-24 11:24:50 +0100

    refactor test_config.py

--- test/env_commands/test_config.py
+++ test/env_commands/test_config.py
@@ -1,22 +1,27 @@
+"""
+  Test for the "apio config" command
+"""
+
+# -- apio config entry point
 from apio.commands.config import cli as cmd_config
 
 
 def test_config(clirunner, validate_cliresult, configenv):
+    """Test "apio config" with different parameters"""
+
     with clirunner.isolated_filesystem():
+
+        # -- Config the environment (conftest.configenv())
         configenv()
+
+        # -- Execute "apio config"
         result = clirunner.invoke(cmd_config)
         validate_cliresult(result)
 
-
-def test_config_list(clirunner, validate_cliresult, configenv):
-    with clirunner.isolated_filesystem():
-        configenv()
+        # -- Execute "apio config --list"
         result = clirunner.invoke(cmd_config, ['--list'])
         validate_cliresult(result)
 
-
-def test_config_exe(clirunner, validate_cliresult, configenv):
-    with clirunner.isolated_filesystem():
-        configenv()
+        # -- Execute "apio config --exe native"
         result = clirunner.invoke(cmd_config, ['--exe', 'native'])
         validate_cliresult(result)
