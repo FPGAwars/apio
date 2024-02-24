7f43b389369a1b99e51e68e9fa6bde9ca5b455d0
Obijuan <obijuan.cube@gmail.com>
2024-02-24 13:04:49 +0100

    refactor test_upgrade.py

--- test/env_commands/test_upgrade.py
+++ test/env_commands/test_upgrade.py
@@ -1,10 +1,26 @@
+"""
+  Test for the "apio upgrade" command
+"""
+
 import pytest
+
+# -- apio upgrade entry point
 from apio.commands.upgrade import cli as cmd_upgrade
 
 
-def test_upgrade(clirunner, validate_cliresult, offline):
+def test_upgrade(clirunner, configenv, validate_cliresult, offline):
+    """Test "apio upgrade" """
+
+    # -- If the option 'offline' is passed, the test is skip
+    # -- (apio upgrade uses internet)
     if offline:
         pytest.skip('requires internet connection')
 
-    result = clirunner.invoke(cmd_upgrade)
-    validate_cliresult(result)
+    with clirunner.isolated_filesystem():
+
+        # -- Config the environment (conftest.configenv())
+        configenv()
+
+        # -- Execute "apio upgrade"
+        result = clirunner.invoke(cmd_upgrade)
+        validate_cliresult(result)
