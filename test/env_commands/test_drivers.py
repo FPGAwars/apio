891c99c633998463c433c2c2c4fc7e7d675a4237
Obijuan <obijuan.cube@gmail.com>
2024-02-24 11:32:18 +0100

    refactor test_drivers.py

--- test/env_commands/test_drivers.py
+++ test/env_commands/test_drivers.py
@@ -1,8 +1,19 @@
+"""
+  Test for the "apio drivers" command
+"""
+
+# -- apio drivers entry point
 from apio.commands.drivers import cli as cmd_drivers
 
 
 def test_drivers(clirunner, validate_cliresult, configenv):
+    """Test "apio drivers" with different parameters"""
+
     with clirunner.isolated_filesystem():
+
+        # -- Config the environment (conftest.configenv())
         configenv()
+
+        # -- Execute "apio drivers"
         result = clirunner.invoke(cmd_drivers)
         validate_cliresult(result)
