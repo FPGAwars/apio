307b3be84181d558f5061a765af09095e9ff8101
Obijuan <obijuan.cube@gmail.com>
2024-02-24 11:14:38 +0100

    refactor test_boads.py

--- test/env_commands/test_boards.py
+++ test/env_commands/test_boards.py
@@ -1,16 +1,27 @@
+"""
+  Test for the "apio boards" command
+"""
+
+# -- apio boards entry point
 from apio.commands.boards import cli as cmd_boards
 
 
-def test_boards(clirunner, validate_cliresult):
-    result = clirunner.invoke(cmd_boards)
-    validate_cliresult(result)
+def test_boards(clirunner, configenv, validate_cliresult):
+    """Test "apio boards" with different parameters"""
 
+    with clirunner.isolated_filesystem():
 
-def test_boards_list(clirunner, validate_cliresult):
-    result = clirunner.invoke(cmd_boards, ['--list'])
-    validate_cliresult(result)
+        # -- Config the environment (conftest.configenv())
+        configenv()
 
+        # -- Execute "apio boards"
+        result = clirunner.invoke(cmd_boards)
+        validate_cliresult(result)
 
-def test_boards_fpgas(clirunner, validate_cliresult):
-    result = clirunner.invoke(cmd_boards, ['--fpga'])
-    validate_cliresult(result)
+        # -- Execute "apio boards --list"
+        result = clirunner.invoke(cmd_boards, ['--list'])
+        validate_cliresult(result)
+
+        # -- Execute "apio boards --fpga"
+        result = clirunner.invoke(cmd_boards, ['--fpga'])
+        validate_cliresult(result)
