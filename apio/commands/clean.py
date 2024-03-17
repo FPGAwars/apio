4f2b462bd04201641ef93028b91770c320b9798e
Obijuan <obijuan.cube@gmail.com>
2024-03-17 19:15:18 +0100

    apio clean refactoring

--- apio/commands/clean.py
+++ apio/commands/clean.py
@@ -1,20 +1,28 @@
-"""Main implementation of APIO CLEAN command"""
-
 # -*- coding: utf-8 -*-
 # -- This file is part of the Apio project
-# -- (C) 2016-2019 FPGAwars
-# -- Author Jesús Arroyo
+# -- (C) 2016-2024 FPGAwars
+# -- Authors
+# --  * Jesús Arroyo (2016-2019)
+# --  * Juan Gonzalez (obijuan) (2019-2024)
 # -- Licence GPLv2
+"""Main implementation of APIO CLEAN command"""
+
 
 from pathlib import Path
-
 import click
-
 from apio.managers.scons import SCons
 from apio import util
 
+# ------------------
+# -- CONSTANTS
+# ------------------
+CMD = "clean"  # -- Comand name
+BOARD = "board"  # -- Option
+PROJECT_DIR = "project_dir"  # -- Option
+VERBOSE = "verbose"  # -- Option
 
-@click.command("clean", context_settings=util.context_settings())
+
+@click.command(CMD, context_settings=util.context_settings())
 @click.pass_context
 @click.option(
     "-p",
@@ -23,15 +31,22 @@ from apio import util
     metavar="str",
     help="Set the target directory for the project.",
 )
-@click.option("-b", "--board", type=str, metavar="str", help="Set the board.")
+@click.option(
+    "-b", f"--{BOARD}", type=str, metavar="str", help="Set the board."
+)
 @click.option(
     "-v",
-    "--verbose",
+    f"--{VERBOSE}",
     is_flag=True,
     help="Show the entire output of the command.",
 )
-def cli(ctx, board, project_dir, verbose):
+def cli(ctx, **kwargs):
     """Clean the previous generated files."""
+
+    # -- Extract the arguments
+    project_dir = kwargs[PROJECT_DIR]
+    board = kwargs[BOARD]
+    verbose = kwargs[VERBOSE]
 
     # -- Create the scons object
     scons = SCons(project_dir)
