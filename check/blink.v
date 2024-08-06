34960f856c325d3fb6e19a9cfc66368a0e435f93
Riley Wilton <riley.j.wilton@gmail.com>
2024-08-05 23:27:58 -0400

    Tring to get gowin boards working.

--- check/blink.v
+++ check/blink.v
@@ -1,17 +1,16 @@
 
 module main (
 	input CLK,
-	output LED,
+	output LED
 );
 
 //-- Modify this value for changing the blink frequency
 localparam N = 22;  //-- N<=21 Fast, N>=23 Slow
 
-reg 
+reg [N:0] counter;
 always @(posedge CLK)
-  r_LED_state <= !r_LED_state;
+  counter <= counter + 1;
 
-assign PMOD1 = r_LED_state;
-assign PMOD2 = r_LED_state2;
+assign LED = counter[N];
 
 endmodule
