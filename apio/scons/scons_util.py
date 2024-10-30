d762bfc0a34c10f27c259bb48b80c4a353f7a3c9
Zapta <zapta@users.noreply.github.com>
2024-10-29 21:53:14 -0700

    Removed the verilator --quiet flag which the verilator rejects. Not sure how it passes the tests when I added it. This fixes the apio lint command.

--- apio/scons/scons_util.py
+++ apio/scons/scons_util.py
@@ -530,7 +530,7 @@ def make_verilator_action(
     """
 
     action = (
-        "verilator --lint-only --quiet --bbox-unsup --timing "
+        "verilator --lint-only --bbox-unsup --timing "
         "-Wno-TIMESCALEMOD -Wno-MULTITOP "
         "{0} {1} {2} {3} {4} {5} {6} {7} {8} $SOURCES"
     ).format(
