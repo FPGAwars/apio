dbf8b5f3461b71e3e78fead3bbd1a38c1cb9f643
Zapta <zapta@users.noreply.github.com>
2024-11-21 15:09:23 -0800

    Cleaned up the 'apio create' test.  (minor changes).

--- test/commands/test_create.py
+++ test/commands/test_create.py
@@ -13,7 +13,7 @@ from apio.commands.create import cli as cmd_create
 
 # R0801: Similar lines in 2 files
 # pylint: disable=R0801
-def check_ini_file(apio_ini: Path, expected_vars: Dict[str, str]) -> None:
+def _check_ini_file(apio_ini: Path, expected_vars: Dict[str, str]) -> None:
     """Assert that apio.ini contains exactly the given vars."""
     # Read the ini file.
     assert isfile(apio_ini)
@@ -52,9 +52,11 @@ def test_create(click_cmd_runner, setup_apio_test_env, assert_apio_cmd_ok):
         # -- Execute "apio create --board icezum"
         result = click_cmd_runner.invoke(cmd_create, ["--board", "icezum"])
         assert_apio_cmd_ok(result)
+        assert "file already exists" not in result.output
+        assert "Do you want to replace it?" not in result.output
         assert "Creating apio.ini file ..." in result.output
         assert "was created successfully." in result.output
-        check_ini_file(apio_ini, {"board": "icezum", "top-module": "main"})
+        _check_ini_file(apio_ini, {"board": "icezum", "top-module": "main"})
 
         # -- Execute "apio create --board alhambra-ii
         # --                      --top-module my_module" with 'y' input"
@@ -68,7 +70,7 @@ def test_create(click_cmd_runner, setup_apio_test_env, assert_apio_cmd_ok):
         assert "file already exists" in result.output
         assert "Do you want to replace it?" in result.output
         assert "was created successfully." in result.output
-        check_ini_file(
+        _check_ini_file(
             apio_ini, {"board": "alhambra-ii", "top-module": "my_module"}
         )
 
@@ -81,7 +83,7 @@ def test_create(click_cmd_runner, setup_apio_test_env, assert_apio_cmd_ok):
         )
         assert_apio_cmd_ok(result)
         assert "was created successfully." in result.output
-        check_ini_file(
+        _check_ini_file(
             apio_ini, {"board": "icezum", "top-module": "my_module"}
         )
 
@@ -97,6 +99,6 @@ def test_create(click_cmd_runner, setup_apio_test_env, assert_apio_cmd_ok):
         assert "file already exists" in result.output
         assert "Do you want to replace it?" in result.output
         assert "Abort!" in result.output
-        check_ini_file(
+        _check_ini_file(
             apio_ini, {"board": "icezum", "top-module": "my_module"}
         )
