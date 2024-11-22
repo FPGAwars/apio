b84aa82d4c5d3357a428a91d600f25f93e19b845
Zapta <zapta@users.noreply.github.com>
2024-11-21 20:23:40 -0800

    Cleaned up the 'apio lint' test.

--- test/commands/test_lint.py
+++ test/commands/test_lint.py
@@ -6,17 +6,23 @@
 from apio.commands.lint import cli as cmd_lint
 
 
-def test_lint(click_cmd_runner, setup_apio_test_env):
-    """Test: apio lint
-    when no apio.ini file is given
-    No additional parameters are given
-    """
+def test_lint_no_packages(
+    click_cmd_runner, setup_apio_test_env, write_apio_ini
+):
+    """Test: apio lint with missing packages."""
 
     with click_cmd_runner.isolated_filesystem():
 
         # -- Config the apio test environment
         setup_apio_test_env()
 
+        # -- Create apio.ini file.
+        write_apio_ini({"board": "icezum", "top-module": "main"})
+
         # -- Execute "apio lint"
-        result = click_cmd_runner.invoke(cmd_lint, ["--board", "alhambra-ii"])
-        assert result.exit_code != 0, result.output
+        result = click_cmd_runner.invoke(cmd_lint)
+        assert result.exit_code == 1, result.output
+        assert (
+            "Error: package 'oss-cad-suite' is not installed" in result.output
+        )
+        assert "apio packages --install --force oss-cad-suite" in result.output
