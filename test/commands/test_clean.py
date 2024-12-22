53405ee2ecd45a5b534920af22ddf84a1822f88e
Zapta <zapta@users.noreply.github.com>
2024-12-22 10:08:05 -0800

    Fixed a test on windows.

--- test/commands/test_clean.py
+++ test/commands/test_clean.py
@@ -3,6 +3,7 @@
 """
 
 from os import chdir
+from os.path import join
 from pathlib import Path
 from test.conftest import ApioRunner
 from apio.commands.clean import cli as apio_clean
@@ -49,7 +50,7 @@ def test_clean_with_apio_ini(apio_runner: ApioRunner):
         result = sb.invoke_apio_cmd(apio_clean)
         assert result.exit_code == 0, result.output
         assert "Removed .sconsign.dblite" in result.output
-        assert "Removed _build/hardware.out" in result.output
+        assert f"Removed {join('_build', 'hardware.out')}" in result.output
         assert "Removed directory _build" in result.output
         assert not Path(".sconsign.dblite").exists()
         assert not Path("_build/hardware.out").exists()
