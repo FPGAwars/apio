2cd73f2f33c6a0cae48776c2a41ffca6e1e34eaa
Zapta <zapta@users.noreply.github.com>
2025-01-12 23:27:52 -0800

    Added a script to check the icestudio test-examples

---
+++ scripts/check-icestudio-test-examples.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Exit on first error.
+set -e
+
+# Change to repo's dir.
+cd ..
+
+# Find all apio.ini in icestudio projects
+projects=$(find test-examples | grep icestudio | grep apio.ini)
+
+for proj in $projects;  do
+    # Get the parent directory of apio.ini
+    proj="$(dirname "$proj")"
+
+    echo
+    echo "--- PROJECT  $proj"
+    echo
+
+    # -- Go to the project's dir.
+    cd $proj
+
+    # -- Exceute apio commands in the project. They should succeeed.
+    set -x
+      apio build
+      apio lint
+      apio test
+      apio graph
+      apio report
+      apio clean
+    set +x
+    
+done
+
+
