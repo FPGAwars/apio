25d9997cb0272021344fc59b1853734d3fa20739
Zapta <zapta@users.noreply.github.com>
2025-01-19 19:31:02 -0800

    Updated the scons proto params definitions.

--- apio/proto/apio_pb2.pyi
+++ apio/proto/apio_pb2.pyi
@@ -18,14 +18,12 @@ ECP5: ApioArch
 GOWIN: ApioArch
 
 class Ice40FpgaInfo(_message.Message):
-    __slots__ = ("fpga_id", "type", "pack")
-    FPGA_ID_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("type", "pack")
     TYPE_FIELD_NUMBER: _ClassVar[int]
     PACK_FIELD_NUMBER: _ClassVar[int]
-    fpga_id: str
     type: str
     pack: str
-    def __init__(self, fpga_id: _Optional[str] = ..., type: _Optional[str] = ..., pack: _Optional[str] = ...) -> None: ...
+    def __init__(self, type: _Optional[str] = ..., pack: _Optional[str] = ...) -> None: ...
 
 class Ecp5FpgaInfo(_message.Message):
     __slots__ = ("type", "pack", "speed")
@@ -70,16 +68,16 @@ class Verbosity(_message.Message):
     def __init__(self, all: bool = ..., synth: bool = ..., pnr: bool = ...) -> None: ...
 
 class Envrionment(_message.Message):
-    __slots__ = ("platform_id", "debug", "yosys_path", "trellis_path")
+    __slots__ = ("platform_id", "is_debug", "yosys_path", "trellis_path")
     PLATFORM_ID_FIELD_NUMBER: _ClassVar[int]
-    DEBUG_FIELD_NUMBER: _ClassVar[int]
+    IS_DEBUG_FIELD_NUMBER: _ClassVar[int]
     YOSYS_PATH_FIELD_NUMBER: _ClassVar[int]
     TRELLIS_PATH_FIELD_NUMBER: _ClassVar[int]
     platform_id: str
-    debug: bool
+    is_debug: bool
     yosys_path: str
     trellis_path: str
-    def __init__(self, platform_id: _Optional[str] = ..., debug: bool = ..., yosys_path: _Optional[str] = ..., trellis_path: _Optional[str] = ...) -> None: ...
+    def __init__(self, platform_id: _Optional[str] = ..., is_debug: bool = ..., yosys_path: _Optional[str] = ..., trellis_path: _Optional[str] = ...) -> None: ...
 
 class Project(_message.Message):
     __slots__ = ("board_id", "top_module")
@@ -90,22 +88,26 @@ class Project(_message.Message):
     def __init__(self, board_id: _Optional[str] = ..., top_module: _Optional[str] = ...) -> None: ...
 
 class CmdLintInfo(_message.Message):
-    __slots__ = ("verilator_all", "verilator_no_style", "verilator_no_warn", "verilator_warn")
+    __slots__ = ("top_module", "verilator_all", "verilator_no_style", "verilator_no_warns", "verilator_warns")
+    TOP_MODULE_FIELD_NUMBER: _ClassVar[int]
     VERILATOR_ALL_FIELD_NUMBER: _ClassVar[int]
     VERILATOR_NO_STYLE_FIELD_NUMBER: _ClassVar[int]
-    VERILATOR_NO_WARN_FIELD_NUMBER: _ClassVar[int]
-    VERILATOR_WARN_FIELD_NUMBER: _ClassVar[int]
+    VERILATOR_NO_WARNS_FIELD_NUMBER: _ClassVar[int]
+    VERILATOR_WARNS_FIELD_NUMBER: _ClassVar[int]
+    top_module: str
     verilator_all: bool
     verilator_no_style: bool
-    verilator_no_warn: _containers.RepeatedScalarFieldContainer[str]
-    verilator_warn: _containers.RepeatedScalarFieldContainer[str]
-    def __init__(self, verilator_all: bool = ..., verilator_no_style: bool = ..., verilator_no_warn: _Optional[_Iterable[str]] = ..., verilator_warn: _Optional[_Iterable[str]] = ...) -> None: ...
+    verilator_no_warns: _containers.RepeatedScalarFieldContainer[str]
+    verilator_warns: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, top_module: _Optional[str] = ..., verilator_all: bool = ..., verilator_no_style: bool = ..., verilator_no_warns: _Optional[_Iterable[str]] = ..., verilator_warns: _Optional[_Iterable[str]] = ...) -> None: ...
 
 class CmdGraphInfo(_message.Message):
-    __slots__ = ("graph_spec",)
+    __slots__ = ("top_module", "graph_spec")
+    TOP_MODULE_FIELD_NUMBER: _ClassVar[int]
     GRAPH_SPEC_FIELD_NUMBER: _ClassVar[int]
+    top_module: str
     graph_spec: str
-    def __init__(self, graph_spec: _Optional[str] = ...) -> None: ...
+    def __init__(self, top_module: _Optional[str] = ..., graph_spec: _Optional[str] = ...) -> None: ...
 
 class CmdSimInfo(_message.Message):
     __slots__ = ("test_bench",)
@@ -119,24 +121,40 @@ class CmdTestInfo(_message.Message):
     test_bench: str
     def __init__(self, test_bench: _Optional[str] = ...) -> None: ...
 
+class CmdUploadInfo(_message.Message):
+    __slots__ = ("programmer_cmd",)
+    PROGRAMMER_CMD_FIELD_NUMBER: _ClassVar[int]
+    programmer_cmd: str
+    def __init__(self, programmer_cmd: _Optional[str] = ...) -> None: ...
+
+class CommandInfo(_message.Message):
+    __slots__ = ("lint", "graph", "sim", "test", "upload")
+    LINT_FIELD_NUMBER: _ClassVar[int]
+    GRAPH_FIELD_NUMBER: _ClassVar[int]
+    SIM_FIELD_NUMBER: _ClassVar[int]
+    TEST_FIELD_NUMBER: _ClassVar[int]
+    UPLOAD_FIELD_NUMBER: _ClassVar[int]
+    lint: CmdLintInfo
+    graph: CmdGraphInfo
+    sim: CmdSimInfo
+    test: CmdTestInfo
+    upload: CmdUploadInfo
+    def __init__(self, lint: _Optional[_Union[CmdLintInfo, _Mapping]] = ..., graph: _Optional[_Union[CmdGraphInfo, _Mapping]] = ..., sim: _Optional[_Union[CmdSimInfo, _Mapping]] = ..., test: _Optional[_Union[CmdTestInfo, _Mapping]] = ..., upload: _Optional[_Union[CmdUploadInfo, _Mapping]] = ...) -> None: ...
+
 class SconsParams(_message.Message):
-    __slots__ = ("arch", "fpga_info", "verbosity", "envrionment", "project", "cmd_lint", "cmd_graph", "cmd_sim", "cmd_test")
+    __slots__ = ("timestamp", "arch", "fpga_info", "verbosity", "envrionment", "project", "cmds")
+    TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
     ARCH_FIELD_NUMBER: _ClassVar[int]
     FPGA_INFO_FIELD_NUMBER: _ClassVar[int]
     VERBOSITY_FIELD_NUMBER: _ClassVar[int]
     ENVRIONMENT_FIELD_NUMBER: _ClassVar[int]
     PROJECT_FIELD_NUMBER: _ClassVar[int]
-    CMD_LINT_FIELD_NUMBER: _ClassVar[int]
-    CMD_GRAPH_FIELD_NUMBER: _ClassVar[int]
-    CMD_SIM_FIELD_NUMBER: _ClassVar[int]
-    CMD_TEST_FIELD_NUMBER: _ClassVar[int]
+    CMDS_FIELD_NUMBER: _ClassVar[int]
+    timestamp: str
     arch: ApioArch
     fpga_info: FpgaInfo
     verbosity: Verbosity
     envrionment: Envrionment
     project: Project
-    cmd_lint: CmdLintInfo
-    cmd_graph: CmdGraphInfo
-    cmd_sim: CmdSimInfo
-    cmd_test: CmdTestInfo
-    def __init__(self, arch: _Optional[_Union[ApioArch, str]] = ..., fpga_info: _Optional[_Union[FpgaInfo, _Mapping]] = ..., verbosity: _Optional[_Union[Verbosity, _Mapping]] = ..., envrionment: _Optional[_Union[Envrionment, _Mapping]] = ..., project: _Optional[_Union[Project, _Mapping]] = ..., cmd_lint: _Optional[_Union[CmdLintInfo, _Mapping]] = ..., cmd_graph: _Optional[_Union[CmdGraphInfo, _Mapping]] = ..., cmd_sim: _Optional[_Union[CmdSimInfo, _Mapping]] = ..., cmd_test: _Optional[_Union[CmdTestInfo, _Mapping]] = ...) -> None: ...
+    cmds: CommandInfo
+    def __init__(self, timestamp: _Optional[str] = ..., arch: _Optional[_Union[ApioArch, str]] = ..., fpga_info: _Optional[_Union[FpgaInfo, _Mapping]] = ..., verbosity: _Optional[_Union[Verbosity, _Mapping]] = ..., envrionment: _Optional[_Union[Envrionment, _Mapping]] = ..., project: _Optional[_Union[Project, _Mapping]] = ..., cmds: _Optional[_Union[CommandInfo, _Mapping]] = ...) -> None: ...
