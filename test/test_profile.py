24e1acbe6992ccef514f65fbc9d56f992ffa9b21
Zapta <zapta@users.noreply.github.com>
2025-02-03 13:28:36 -0800

    Added initial profile test.

---
+++ test/test_profile.py
@@ -0,0 +1,42 @@
+"""
+Tests of apio_profile.py
+"""
+
+import json
+from test.conftest import ApioRunner
+from apio.profile import Profile
+
+TEST_DATA = {
+    "installed-packages": {
+        "examples": {"version": "0.1.4"},
+        "verible": {"version": "0.0.1"},
+    },
+    "preferences": {"theme": "light"},
+    "remote-config": {
+        "packages": {
+            "drivers": {"version": "1.2.0"},
+            "examples": {"version": "0.1.4"},
+        }
+    },
+    "settings": {"macos_ftdi_drivers": False},
+}
+
+
+def test_profile_loading(apio_runner: ApioRunner):
+    """Tests the loading and validation of a profile file."""
+
+    with apio_runner.in_sandbox() as sb:
+
+        # -- Write a test profile.json file.
+        path = sb.home_dir / "profile.json"
+        sb.write_file(path, json.dumps(TEST_DATA, indent=2))
+
+        # -- Read back the contnt.
+        profile = Profile(sb.home_dir, "http://fake-domain.com/config")
+
+        # -- Verify
+        assert profile.preferences == TEST_DATA["preferences"]
+        assert profile.settings == TEST_DATA["settings"]
+        assert profile.packages == TEST_DATA["installed-packages"]
+        assert profile.remote_config == TEST_DATA["remote-config"]
+        assert not profile.remote_config_fetched
