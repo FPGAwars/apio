b768e5323e3a4adccc6ce10b61a038ccf3f7dd0e
Zapta <zapta@users.noreply.github.com>
2025-05-26 20:44:18 -0700

    Cleaned up the 'apio labs scan-usb' command.

--- COMMANDS.md
+++ COMMANDS.md
@@ -33,8 +33,7 @@
     * [apio info resources](#apio-info-resources) - Additional resources.
     * [apio info system](#apio-info-system) - Show system information.
   * [apio labs](#apio-labs) - Experimental apio commands.
-    * [apio labs ftdi-scan](#apio-labs-ftdi-scan) - An experimental command to scan FTDI devices.
-    * [apio labs usb-scan](#apio-labs-usb-scan) - An experimental command to scan USB devices.
+    * [apio labs scan-usb](#apio-labs-scan-usb) - An experimental command to scan USB devices.
   * [apio lint](#apio-lint) - Lint the source code.
   * [apio packages](#apio-packages) - Manage the apio packages.
     * [apio packages fix](#apio-packages-fix) - Fix broken apio packages.
@@ -889,48 +888,25 @@ Options:
   -h, --help  Show this message and exit.
 
 Subcommands:
-  apio labs usb-scan   An experimental command to scan USB devices.
-  apio labs ftdi-scan  An experimental command to scan FTDI devices.
+  apio labs scan-usb  An experimental command to scan USB devices.
 
 ```
 
 <br>
 
-### apio labs ftdi-scan
+### apio labs scan-usb
 
 ```
-Usage: apio labs ftdi-scan [OPTIONS]
+Usage: apio labs scan-usb [OPTIONS]
 
-  The command 'apio labs ftdi-scan' is a temporary command that is used
+  The command 'apio labs scan-usb' is a temporary command that is used
   to evaluate a new way to scan USB devices connected to the host
   system. It is not part of the official apio command set and most
   likely will change or be removed in the future.
 
   Examples:
-    apio labs ftdi-scan     # Scan and print FTDI devices
-    apio labs ftdi-scan -v  # With extra info
-
-Options:
-  -v, --verbose  Show detailed output.
-  -h, --help     Show this message and exit.
-
-```
-
-<br>
-
-### apio labs usb-scan
-
-```
-Usage: apio labs usb-scan [OPTIONS]
-
-  The command 'apio labs usb-scan' is a temporary command that is used
-  to evaluate a new way to scan USB devices connected to the host
-  system. It is not part of the official apio command set and most
-  likely will change or be removed in the future.
-
-  Examples:
-    apio labs usb-scan     # Scan and print USB devices
-    apio labs usb-scan -v  # With extra info
+    apio labs scan-usb     # Scan and print USB devices
+    apio labs scan-usb -v  # With extra info
 
 Options:
   -v, --verbose  Show detailed output.
--- apio/commands/apio_labs.py
+++ apio/commands/apio_labs.py
@@ -11,9 +11,13 @@ import os
 from typing import Optional, List
 from dataclasses import dataclass
 import click
+
+# from pyftdi.ftdi import Ftdi
 from rich.table import Table
 from rich import box
 import usb.core
+
+# from pyftdi.usbtools import UsbDeviceDescriptor
 from apio.managers import installer
 from apio.commands import options
 from apio.common.apio_console import cout, cprint
@@ -37,7 +41,7 @@ FTDI_PID_TO_MODEL = {
     0x8374: "FT4232",  # Early quad interface device
 }
 
-# ------ apio labs usb-scan
+# ------ apio labs scan-usb
 
 
 @dataclass()
@@ -53,7 +57,8 @@ class UsbDevice:
     manufacturer: str
     description: str
     serial_num: str
-    ftdi_type: str
+    comment: str
+    assessable: bool
 
 
 def get_usb_str(
@@ -71,6 +76,17 @@ def get_usb_str(
         if verbose:
             print(f"Error getting USB string at index {index}: {e}")
         return default
+
+
+def is_device_accessible(dev, verbose: bool):
+    """Check if the device is accessible via libusb"""
+    try:
+        dev.set_configuration()  # Attempt to claim device
+        return True
+    except usb.core.USBError as e:
+        if verbose:
+            print(f"Accessability exception: {e}")
+        return False
 
 
 def get_usb_devices(apio_ctx: ApioContext, verbose: bool) -> List[UsbDevice]:
@@ -99,17 +115,24 @@ def get_usb_devices(apio_ctx: ApioContext, verbose: bool) -> List[UsbDevice]:
         # -- Sanity check.
         assert isinstance(device, usb.core.Device), type(device)
 
-        # -- Skip hubs. We don't care about them.
-        if device.bDeviceClass == 0x09:
-            continue
+        # -- Determine if this is a hub.
+        is_hub = device.bDeviceClass == 0x09
 
         # -- Determine ftdi type.
-        if device.idVendor == 0x0403:
+        if is_hub:
+            comment = "Hub"
+        elif device.idVendor == 0x0403:
             # -- This is an FTDI device, so look up its type by its PID.
-            ftdi_type = FTDI_PID_TO_MODEL.get(device.idProduct, "UNKNOWN")
+            comment = FTDI_PID_TO_MODEL.get(device.idProduct, "UNKNOWN")
         else:
             # -- Not an FTDI device, so no type.
-            ftdi_type = ""
+            comment = ""
+
+        # -- Check if it's has a libusb compatible driver.
+        # -- We skip this check for hubs.
+        accessible = (not is_hub) and is_device_accessible(
+            device, verbose=verbose
+        )
 
         # -- Create the device object.
         item = UsbDevice(
@@ -129,7 +152,8 @@ def get_usb_devices(apio_ctx: ApioContext, verbose: bool) -> List[UsbDevice]:
             serial_num=get_usb_str(
                 device, device.iSerialNumber, default="", verbose=verbose
             ),
-            ftdi_type=ftdi_type,
+            comment=comment,
+            assessable=accessible,
         )
         result.append(item)
 
@@ -141,30 +165,30 @@ def get_usb_devices(apio_ctx: ApioContext, verbose: bool) -> List[UsbDevice]:
 
 
 # -- Text in the rich-text format of the python rich library.
-APIO_API_LABS_USB_SCAN_HELP = """
-The command 'apio labs usb-scan' is a temporary command that is used \
+APIO_API_LABS_SCAN_USB_HELP = """
+The command 'apio labs scan-usb' is a temporary command that is used \
 to evaluate a new way to scan USB devices connected to the host \
 system. It is not part of the official apio command set and \
 most likely will change or be removed in the future.
 
 Examples:[code]
-  apio labs usb-scan     # Scan and print USB devices
-  apio labs usb-scan -v  # With extra info[/code]
+  apio labs scan-usb     # Scan and print USB devices
+  apio labs scan-usb -v  # With extra info[/code]
 """
 
 
 @click.command(
-    name="usb-scan",
+    name="scan-usb",
     cls=ApioCommand,
     short_help="An experimental command to scan USB devices.",
-    help=APIO_API_LABS_USB_SCAN_HELP,
+    help=APIO_API_LABS_SCAN_USB_HELP,
 )
 @options.verbose_option
-def _usb_scan_cli(
+def _scan_usb_cli(
     # -- Options
     verbose: bool,
 ):
-    """Implements the 'apio labs usb-scan' command."""
+    """Implements the 'apio labs scan-usb' command."""
 
     apio_ctx = ApioContext(scope=ApioContextScope.NO_PROJECT)
 
@@ -190,26 +214,28 @@ def _usb_scan_cli(
     )
 
     # -- Add columns
-    table.add_column("BUS", no_wrap=True, justify="center")
-    table.add_column("DEV", no_wrap=True, justify="center")
-    table.add_column("VID", no_wrap=True)
-    table.add_column("PID", no_wrap=True)
+    table.add_column("BUS:DEV", no_wrap=True, justify="center")
+    # table.add_column("DEV", no_wrap=True, justify="center")
+    table.add_column("VID:PID", no_wrap=True)
+    # table.add_column("PID", no_wrap=True)
     table.add_column("MANUFACTURER", no_wrap=True, style=EMPH3)
     table.add_column("DESCRIPTION", no_wrap=True, style=EMPH3)
     table.add_column("SERIAL-NUM", no_wrap=True)
-    table.add_column("FTDI-TYPE", no_wrap=True)
+    table.add_column("ACCESS", no_wrap=True, justify="center")
+    table.add_column("COMMENT", no_wrap=True)
 
     # -- Add a raw per device
     for device in devices:
         values = []
-        values.append(str(device.bus))
-        values.append(str(device.device))
-        values.append(f"{device.vendor_id:04X}")
-        values.append(f"{device.product_id:04X}")
+        values.append(f"{device.bus}:{device.device}")
+        # values.append(f"{device.device:02d}")
+        values.append(f"{device.vendor_id:04X}:{device.product_id:04X}")
+        # values.append(f"{device.product_id:04X}")
         values.append(device.manufacturer)
         values.append(device.description)
         values.append(device.serial_num)
-        values.append(device.ftdi_type)
+        values.append("Y" if device.assessable else "")
+        values.append(device.comment)
 
         # -- Add row.
         table.add_row(*values)
@@ -218,204 +244,6 @@ def _usb_scan_cli(
     cout()
     cprint(table)
     cout(f"Found {util.plurality(devices, 'USB device')}", style=SUCCESS)
-
-
-# ------ apio labs ftdi-scan
-
-
-@dataclass()
-class FtdiDevice:
-    """A data class to hold the information of a single FTDI device."""
-
-    # pylint: disable=too-many-instance-attributes
-
-    bus: int
-    device: int
-    vendor_id: str
-    product_id: str
-    manufacturer: str
-    description: str
-    serial_num: str
-    ftdi_type: str
-
-
-# # -- Mapping of FTDI PID to IC model number. Try to keep it consistent with
-# # -- src/libusb_ll.cpp#L159 at https://github.com/trabucayre/openFPGALoader.
-# FTDI_PID_TO_MODEL = {
-#     0x6001: "FT232R",  # Most common UART interface
-#     0x6010: "FT2232H",  # Dual channel, high-speed
-#     0x6011: "FT4232H",  # Quad channel, high-speed
-#     0x6014: "FT232H",  # Single channel, high-speed, multi-protocol
-#     0x6015: "FTX Series",  # Includes FT231X, FT234X (basic UART)
-#     0x6017: "FT313H",  # USB host controller IC
-#     0x8372: "FT245R",  # Parallel FIFO interface
-#     0x8371: "FT232BM",  # Older UART chip
-#     0x8373: "FT2232C",  # Early dual interface device
-#     0x8374: "FT4232",  # Early quad interface device
-# }
-
-
-# def get_usb_str(
-#     device: usb.core.Device, index: int, default: str, verbose: bool
-# ) -> Optional[str]:
-#     """Extract usb string by its index."""
-#     try:
-#         s = usb.util.get_string(device, index)
-#         # For Tang 9K which contains a null char as a string separator.
-#         # It's not USB standard but C tools do that implicitly.
-#         s = s.split("\x00", 1)[0]
-#         return s
-#     except Exception as e:
-#         if verbose:
-#             print(f"Error getting USB string at index {index}: {e}")
-#         return default
-
-
-def get_ftdi_devices(apio_ctx: ApioContext, verbose: bool) -> List[FtdiDevice]:
-    """Query and return a list with ftdi device info."""
-
-    lib_path = str(apio_ctx.get_package_dir("oss-cad-suite") / "lib")
-
-    if verbose:
-        print(f"{lib_path=}")
-
-    # TODO: prepend to path if has prior value.
-    os.environ["DYLD_LIBRARY_PATH"] = lib_path
-
-    # -- Find the usb devices.
-    # devices: List[usb.core.Device] = usb.core.find(find_all=True)
-
-    # -- Collect the devices
-    result: List[FtdiDevice] = []
-    # for device in devices:
-    #     # -- Print entire device info for debugging.
-    #     if verbose:
-    #         cout()
-    #         cout(str(device))
-    #         cout()
-
-    #     # -- Sanity check.
-    #     assert isinstance(device, usb.core.Device), type(device)
-
-    #     # -- Skip hubs. We don't care about them.
-    #     if device.bDeviceClass == 0x09:
-    #         continue
-
-    #     # -- Determine ftdi type.
-    #     if device.idVendor == 0x0403:
-    #         # -- This is an FTDI device, so look up its type by its PID.
-    #         ftdi_type = FTDI_PID_TO_MODEL.get(device.idProduct, "UNKNOWN")
-    #     else:
-    #         # -- Not an FTDI device, so no type.
-    #         ftdi_type = ""
-
-    #     # -- Create the device object.
-    #     item = UsbDevice(
-    #         bus=device.bus,
-    #         device=device.address,
-    #         vendor_id=device.idVendor,
-    #         product_id=device.idProduct,
-    #         manufacturer=get_usb_str(
-    #             device,
-    #             device.iManufacturer,
-    #             default="(unavail)",
-    #             verbose=verbose,
-    #         ),
-    #         description=get_usb_str(
-    #             device, device.iProduct, default="(unavail)", verbose=verbose
-    #         ),
-    #         serial_num=get_usb_str(
-    #             device, device.iSerialNumber, default="", verbose=verbose
-    #         ),
-    #         ftdi_type=ftdi_type,
-    #     )
-    #     result.append(item)
-
-    # # -- Sort by (bus, device).
-    # result = sorted(result, key=lambda d: (d.bus, d.device))
-
-    # -- All done.
-    return result
-
-
-# -- Text in the rich-text format of the python rich library.
-APIO_API_LABS_FTDI_SCAN_HELP = """
-The command 'apio labs ftdi-scan' is a temporary command that is used \
-to evaluate a new way to scan USB devices connected to the host \
-system. It is not part of the official apio command set and \
-most likely will change or be removed in the future.
-
-Examples:[code]
-  apio labs ftdi-scan     # Scan and print FTDI devices
-  apio labs ftdi-scan -v  # With extra info[/code]
-"""
-
-
-@click.command(
-    name="ftdi-scan",
-    cls=ApioCommand,
-    short_help="An experimental command to scan FTDI devices.",
-    help=APIO_API_LABS_FTDI_SCAN_HELP,
-)
-@options.verbose_option
-def _ftdi_scan_cli(
-    # -- Options
-    verbose: bool,
-):
-    """Implements the 'apio labs ftdi-scan' command."""
-
-    apio_ctx = ApioContext(scope=ApioContextScope.NO_PROJECT)
-
-    # -- Prepare the packages for use.
-    installer.install_missing_packages_on_the_fly(apio_ctx)
-
-    # -- Get the list of devices
-    devices = get_ftdi_devices(apio_ctx, verbose=verbose)
-
-    # -- If not found, print a message and exit.
-    if not devices:
-        cout("No FTDI devices found.", style=ERROR)
-        return
-
-    # -- Define the table.
-    table = Table(
-        show_header=True,
-        show_lines=True,
-        box=box.SQUARE,
-        border_style=BORDER,
-        title="FTDI Devices",
-        title_justify="left",
-    )
-
-    # -- Add columns
-    table.add_column("BUS", no_wrap=True, justify="center")
-    table.add_column("DEV", no_wrap=True, justify="center")
-    table.add_column("VID", no_wrap=True)
-    table.add_column("PID", no_wrap=True)
-    table.add_column("MANUFACTURER", no_wrap=True, style=EMPH3)
-    table.add_column("DESCRIPTION", no_wrap=True, style=EMPH3)
-    table.add_column("SERIAL-NUM", no_wrap=True)
-    table.add_column("FTDI-TYPE", no_wrap=True)
-
-    # -- Add a raw per device
-    for device in devices:
-        values = []
-        values.append(str(device.bus))
-        values.append(str(device.device))
-        values.append(f"{device.vendor_id:04X}")
-        values.append(f"{device.product_id:04X}")
-        values.append(device.manufacturer)
-        values.append(device.description)
-        values.append(device.serial_num)
-        values.append(device.ftdi_type)
-
-        # -- Add row.
-        table.add_row(*values)
-
-    # -- Render the table.
-    cout()
-    cprint(table)
-    cout(f"Found {util.plurality(devices, 'FTDI device')}", style=SUCCESS)
 
 
 # ------ apio apio
@@ -432,8 +260,8 @@ SUBGROUPS = [
     ApioSubgroup(
         "Subcommands",
         [
-            _usb_scan_cli,
-            _ftdi_scan_cli,
+            _scan_usb_cli,
+            # _ftdi_scan_cli,
         ],
     )
 ]
