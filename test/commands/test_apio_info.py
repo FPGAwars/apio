4162bd64030bf658a041ee2046c1567f2db29812
Zapta <zapta@users.noreply.github.com>
2025-06-01 10:16:31 -0700

    Updated platforms name.s

--- test/commands/test_apio_info.py
+++ test/commands/test_apio_info.py
@@ -30,7 +30,7 @@ def test_apio_info(apio_runner: ApioRunner):
         result = sb.invoke_apio_cmd(apio, ["info", "platforms"])
         assert result.exit_code == 0, result.output
         assert "darwin_arm64" in result.output
-        assert "Mac OSX, ARM 64 bit" in result.output
+        assert "Mac OSX, ARM 64 bit (Apple Silicon)" in result.output
 
         # -- Execute "apio info colors"
         result = sb.invoke_apio_cmd(apio, ["info", "colors"])
