FPGAwars/apio

commit 38c2e084df48069630cfaaa4acee0a4c10f17694

Fix sim error about "Unknown module type: EHXPLLL" with ECP5.
- Details documented in issue: https://github.com/FPGAwars/icestudio/issues/542
- It has been fixed in pull request https://github.com/FPGAwars/apio/pull/374 and merged. However the change seems lost during later refactoring.

apio/scons/plugin_ecp5.py
@@ -44,6 +44,7 @@ class PluginEcp5(PluginBase):
         self.database_path = trellis_path / "database"
         self.yosys_lib_dir = yosys_path / "ecp5"
         self.yosys_lib_file = yosys_path / "ecp5" / "cells_sim.v"
+        self.yosys_lib_exclude_file = yosys_path / "ecp5" / "cells_bb.v"
 
     def plugin_info(self) -> ArchPluginInfo:
         """Return plugin specific parameters."""
@@ -156,6 +157,7 @@ class PluginEcp5(PluginBase):
                     is_interactive=apio_env.targeting("sim"),
                     lib_dirs=[self.yosys_lib_dir],
                     lib_files=[self.yosys_lib_file],
+                    extra_params=["-D NOT_INCLUDES {}".format(self.yosys_lib_exclude_file)]
                 ),
             ]
             return action

commit 4b14f896234dd2f78ff4997fd1d3f5bc90ca2aa8

Commented out a commit 38c2e084df48069630cfaaa4acee0a4c10f17694 which broke the test. Will keep working on it in followup commits.

apio/scons/plugin_ecp5.py
@@ -44,7 +44,7 @@ class PluginEcp5(PluginBase):
         self.database_path = trellis_path / "database"
         self.yosys_lib_dir = yosys_path / "ecp5"
         self.yosys_lib_file = yosys_path / "ecp5" / "cells_sim.v"
-        self.yosys_lib_exclude_file = yosys_path / "ecp5" / "cells_bb.v"
+        # self.yosys_lib_exclude_file = yosys_path / "ecp5" / "cells_bb.v"
 
     def plugin_info(self) -> ArchPluginInfo:
         """Return plugin specific parameters."""
@@ -157,7 +157,8 @@ class PluginEcp5(PluginBase):
                     is_interactive=apio_env.targeting("sim"),
                     lib_dirs=[self.yosys_lib_dir],
                     lib_files=[self.yosys_lib_file],
-                    extra_params=["-D NOT_INCLUDES {}".format(self.yosys_lib_exclude_file)]
+                    # extra_params=["-D NOT_INCLUDES {}"
+                    #    .format(self.yosys_lib_exclude_file)]
                 ),
             ]
             return action