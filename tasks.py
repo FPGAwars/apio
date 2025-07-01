917f84467ba2597fcf0fb40ef4be848530c213d3
Zapta <zapta@users.noreply.github.com>
2025-06-30 20:28:42 -0700

    Added additional invoke tasks. This complete the initial task list.

--- tasks.py
+++ tasks.py
@@ -19,11 +19,16 @@ LATEST_PYTHON = "py313"
 # -- The python interpreter that we currently use.
 PYTHON = sys.executable
 
-# -- Auto install the package 'tox' if not already installed.
-if importlib.util.find_spec("tox") is None:
-    out("[magenta]Auto installing the 'tox' package.[/]")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "tox"])
-assert importlib.util.find_spec("tox") is not None, "'tox' is not installed."
+# -- Auto install packages that are not Apio dependencies but required here.
+modules = ["tox", "flit"]
+for module in modules:
+    if importlib.util.find_spec(module) is None:
+        out(f"[magenta]Auto installing package '{module}'.[/]")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", module])
+    assert (
+        importlib.util.find_spec(module) is not None
+    ), f"'{module}' is not installed."
+
 
 # ---------- Task 'lint' ----------
 
@@ -70,4 +75,74 @@ def check_task(ctx):
         "--skip-missing-interpreters false "
         f"-e lint,{LATEST_PYTHON}"
     )
+    ctx.run(cmd, pty=True)
+
+
+# ---------- Task 'check-all' ----------
+
+
+@task(
+    name="check-all",
+    aliases=["ca"],
+)
+def check_all_task(ctx):
+    """Lint and all tests using all Python versions."""
+    cmd = f"{PYTHON} -m tox " "--skip-missing-interpreters false "
+    ctx.run(cmd, pty=True)
+
+
+# ---------- Task 'test-coverage' ----------
+
+
+@task(
+    name="test-coverage",
+    aliases=["tc"],
+)
+def test_coverage_task(ctx):
+    """Generate test coverage report."""
+    cmd = (
+        f"{PYTHON} -m tox --skip-missing-interpreters false "
+        f"-e {LATEST_PYTHON} "
+        "-- --cov --cov-report=html:_pytest-coverage"
+    )
+    ctx.run(cmd, pty=True)
+
+
+# ---------- Task 'publish-test' ----------
+
+
+@task(name="publish-test")
+def publish_test_task(ctx):
+    """Publish to test test Pypi."""
+    cmd = f"{PYTHON} -m flit publish --repository testpypi"
+    ctx.run(cmd, pty=True)
+
+
+# ---------- Task 'publish' ----------
+
+
+@task(name="publish")
+def publish_task(ctx):
+    """Publish to ptod Pypi."""
+    cmd = f"{PYTHON} -m flit publish"
+    ctx.run(cmd, pty=True)
+
+
+# ---------- Task 'install-apio' ----------
+
+
+@task(name="install-apio", aliases=["ia"])
+def install_apio_task(ctx):
+    """Install apio package from source code."""
+    cmd = f"{PYTHON} -m pip install -e ."
+    ctx.run(cmd, pty=True)
+
+
+# ---------- Task 'uninstall-apio' ----------
+
+
+@task(name="uninstall-apio", aliases=["ua"])
+def uninstall_apio_task(ctx):
+    """Uninstall the apio package."""
+    cmd = f"{PYTHON} -m pip uninstall apio"
     ctx.run(cmd, pty=True)
