b040d11ac4353469d186f868e8529e0d0d103ef5
Zapta <zapta@users.noreply.github.com>
2025-10-30 14:09:09 -0700

    Updated test coverage example to use Popen.

--- experimental/test-coverage/foo.py
+++ experimental/test-coverage/foo.py
@@ -13,10 +13,25 @@ def launch_child():
     print(f"*** Parent: SConstruct = {sconst_path}")
 
     # -- Change dir away from the sconstruct file,
-    os.chdir("..")
+    os.chdir("/tmp")
 
     # -- Run the scons subprocess
     subprocess.run([sys.executable, "-m", "SCons", "-f", sconst_path])
+
+    proc = subprocess.Popen(
+        [sys.executable, "-m", "SCons", "-f", sconst_path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+
+    stdout, stderr = proc.communicate()
+
+    assert proc.returncode == 0
+
+    # print(stdout)
+    # print(stderr)
+
     print("*** Parent: back from child.")
 
 
@@ -24,6 +39,3 @@ def called_from_scons():
     """Called from SConstruct to print a message"""
     print("*** Child: called from scons")
     print(f"*** Child: current dir: {os.getcwd()}")
-
-
-
