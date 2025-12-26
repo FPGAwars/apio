0cde855640d913e7cb289ae64fc2280606cdbc69
Zapta <zapta@users.noreply.github.com>
2025-12-25 17:12:52 -0800

    Adapted the repo to the switch from 'develop' branch (now frozen) to 'main' branch. Remove old remote config files that are fetched from 'develop' branch and replaced the branch name in a few places.

--- apio/__init__.py
+++ apio/__init__.py
@@ -11,7 +11,7 @@
 # --------------------------------------------
 
 # -- Developer: Change this number when releasing a new version
-VERSION = (1, 1, 0)
+VERSION = (1, 2, 0)
 
 # -- Get the version as a string. Ex: "0.10.1"
 __version__ = ".".join([str(s) for s in VERSION])
