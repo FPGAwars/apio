22fbdc5156c57f84e97af52d4911701b3a526aec
Zapta <zapta@users.noreply.github.com>
2026-01-27 16:10:33 -0800

    Experimental work. Does not affect apio.

--- experimental/ecp5_lint/lint.sh
+++ experimental/ecp5_lint/lint.sh
@@ -10,6 +10,8 @@ echo "Running verilator"
 
 verilator_bin --lint-only --bbox-unsup --timing -Wno-TIMESCALEMOD -Wno-MULTITOP _fixed_new_cells_bb.v
 
+#verilator_bin --lint-only --bbox-unsup --timing -Wno-TIMESCALEMOD -Wno-MULTITOP yosys_fixed_cells_bb_ecp5.v
+
 echo "Exit code = $?"
 
 
