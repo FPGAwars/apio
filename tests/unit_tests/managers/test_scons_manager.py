7e6b37e09f17e95c09e03c5039df801e347d6307
obijuan <obijuan.cube@gmail.com>
2026-06-13 18:21:46 +0200

    Fix bug: pass test_explicit_params

--- tests/unit_tests/managers/test_scons_manager.py
+++ tests/unit_tests/managers/test_scons_manager.py
@@ -154,12 +154,6 @@ def test_default_params(apio_runner: ApioRunner):
             sb.packages_dir / "openxc7/chipdb"
         )
 
-        # -- DEBUG:
-        print("---- scons_params --------")
-        print(scons_params)
-        print("----- expected ---------")
-        print(expected)
-
         # -- Compare actual to expected values.
         assert str(scons_params) == str(expected)
 
@@ -202,8 +196,14 @@ def test_explicit_params(apio_runner: ApioRunner):
         )
         expected.environment.scons_shell_id = apio_ctx.scons_shell_id
 
+        # -- TODO: For the missing platforms it should be the null string ""
         expected.environment.prjxray_db_path = str(
             sb.packages_dir / "openxc7/share/nextpnr/external/prjxray-db"
+        )
+
+        # -- TODO: For the missing platforms it should be the null string ""
+        expected.environment.chipdb_path = str(
+            sb.packages_dir / "openxc7/chipdb"
         )
 
         # -- Compare actual to expected values.
